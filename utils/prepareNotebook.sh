6f5048ba1c18ab0e2a05d927057e262c408708dc
Robin Long <binlon@amazon.com>
2023-07-17 18:54:50 +0800

    update bashrc

--- utils/prepareNotebook.sh
+++ utils/prepareNotebook.sh
@@ -104,10 +104,6 @@ echo "==============================================="
 echo "  Install Go ......"
 echo "==============================================="
 sudo yum install golang -y
-export GOPATH=$(go env GOPATH)
-echo 'export GOPATH='${GOPATH} >> ~/.bashrc
-echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
-source ~/.bashrc
 go version
 
 
@@ -115,10 +111,6 @@ echo "==============================================="
 echo "  Install ccat ......"
 echo "==============================================="
 go install github.com/owenthereal/ccat@latest
-cat >> ~/.bashrc <<EOF
-alias cat=ccat
-EOF
-source ~/.bashrc
 
 
 echo "==============================================="
@@ -250,7 +242,6 @@ pip install jupyterlab_execute_time
 source deactivate
 
 
-
 echo "==============================================="
 echo " Persistant Conda ......"
 echo "==============================================="
@@ -316,7 +307,9 @@ wget https://raw.githubusercontent.com/aws-samples/amazon-sagemaker-notebook-ins
 echo "==============================================="
 echo "  Shell Scripts ......"
 echo "==============================================="
-# echo "Create sh profile  ..."
+echo "Donwload sh scripts ..."
+wget https://raw.githubusercontent.com/AIMLTOP/awesome-sagemaker/main/utils/bashrc -O /home/ec2-user/SageMaker/custom/bashrc
+echo 'export PATH=$PATH:/home/ec2-user/SageMaker/custom' >> ~/.bashrc
 # echo "alias b='/bin/bash'" > ~/.profile
 # source ~/.profile
 # echo "alias c='clear'" | tee -a ~/.bashrc
@@ -329,61 +322,9 @@ echo "==============================================="
 # sudo systemctl --no-block restart jupyter-server.service
 # source ~/.bashrc
 
-sudo bash -c "cat << EOF > /usr/local/bin/b
-#!/bin/bash
-/bin/bash
-EOF"
-sudo chmod +x /usr/local/bin/b
-
-sudo bash -c "cat > /usr/local/bin/rc <<EOF
-#!/bin/bash
-echo \"alias c='clear'\" | tee -a ~/.bashrc
-echo \"alias b='/bin/bash'\" | tee -a ~/.bashrc
-echo \"alias cs='cd /home/ec2-user/SageMaker'\" | tee -a ~/.bashrc
-echo \"alias cls='conda env list'\" | tee -a ~/.bashrc
-echo \"alias sa='source activate JupyterSystemEnv'\" | tee -a ~/.bashrc
-echo \"alias sd='source deactivate'\" | tee -a ~/.bashrc
-echo \"alias rr='sudo systemctl daemon-reload; sudo systemctl restart jupyter-server'\" | tee -a ~/.bashrc
-
-echo export GOPATH=\\\$(go env GOPATH) | tee -a ~/.bashrc
-echo export PATH=\\\$PATH:\\\$GOPATH/bin | tee -a ~/.bashrc
-
-echo \"export PYENV_ROOT=\\\$HOME/.pyenv\" | tee -a ~/.bashrc
-echo \"export PATH=\\\$HOME/.pyenv/bin\:\\\$PATH\" | tee -a ~/.bashrc
-eval \"\\\$(\\\$HOME/.pyenv/bin/pyenv init -)\"
-
-unset SUDO_UID
-WORKING_DIR=/home/ec2-user/SageMaker/custom
-source \"\\\$WORKING_DIR/miniconda/bin/activate\"
-
-for env in \\\$WORKING_DIR/miniconda/envs/*; do
-    BASENAME=\\\$(basename \"\\\$env\")
-    conda activate \"\\\$BASENAME\"
-    python -m ipykernel install --user --name \"\\\$BASENAME\"
-done
-
-IDLE_TIME=10800
-CONDA_PYTHON_DIR=\\\$(source /home/ec2-user/anaconda3/bin/activate /home/ec2-user/anaconda3/envs/JupyterSystemEnv && which python)
-if \\\$CONDA_PYTHON_DIR -c \"import boto3\" 2>/dev/null; then
-    PYTHON_DIR=\\\$CONDA_PYTHON_DIR
-elif /usr/bin/python -c \"import boto3\" 2>/dev/null; then
-    PYTHON_DIR='/usr/bin/python'
-else
-    echo \"No boto3 found in Python or Python3. Exiting...\"
-    exit 1
-fi
-echo \"Starting the SageMaker autostop script in cron\"
-(crontab -l 2>/dev/null; echo \"*/5 * * * * \\\$PYTHON_DIR /home/ec2-user/SageMaker/custom/autostop.py --time \\\$IDLE_TIME --ignore-connections >> /var/log/jupyter.log\") | crontab -
-EOF"
-sudo chmod +x /usr/local/bin/rc
-rc
-source ~/.bashrc
-
-# 不加 display-name，避免重复
-# python -m ipykernel install --user --name \"\\\$BASENAME\" --display-name \"Custom (\\\$BASENAME)\"
-
 
 # 最后再执行一次 source
+sudo chmod +x /home/ec2-user/SageMaker/custom/bashrc
 sudo chmod +x /home/ec2-user/SageMaker/custom/*.sh
 sudo chown ec2-user:ec2-user /home/ec2-user/SageMaker/custom/ -R
 echo "source ~/.bashrc"
--- utils/utils.md
+++ utils/utils.md
@@ -1,5 +1,71 @@
 
 
+## Bash in Bash
+
+Simple Example
+```shell
+sudo bash -c "cat << EOF > /usr/local/bin/b
+#!/bin/bash
+/bin/bash
+EOF"
+sudo chmod +x /usr/local/bin/b
+```
+
+
+Complex Example
+```shell
+sudo bash -c "cat > /usr/local/bin/rc <<EOF
+#!/bin/bash
+echo \"alias c='clear'\" | tee -a ~/.bashrc
+echo \"alias b='/bin/bash'\" | tee -a ~/.bashrc
+echo \"alias cs='cd /home/ec2-user/SageMaker'\" | tee -a ~/.bashrc
+echo \"alias cls='conda env list'\" | tee -a ~/.bashrc
+echo \"alias sa='source activate JupyterSystemEnv'\" | tee -a ~/.bashrc
+echo \"alias sd='source deactivate'\" | tee -a ~/.bashrc
+echo \"alias rr='sudo systemctl daemon-reload; sudo systemctl restart jupyter-server'\" | tee -a ~/.bashrc
+
+echo export GOPATH=\\\$(go env GOPATH) | tee -a ~/.bashrc
+echo export PATH=\\\$PATH:\\\$GOPATH/bin | tee -a ~/.bashrc
+
+echo \"export PYENV_ROOT=\\\$HOME/.pyenv\" | tee -a ~/.bashrc
+echo \"export PATH=\\\$HOME/.pyenv/bin\:\\\$PATH\" | tee -a ~/.bashrc
+eval \"\\\$(\\\$HOME/.pyenv/bin/pyenv init -)\"
+
+unset SUDO_UID
+WORKING_DIR=/home/ec2-user/SageMaker/custom
+source \"\\\$WORKING_DIR/miniconda/bin/activate\"
+
+for env in \\\$WORKING_DIR/miniconda/envs/*; do
+    BASENAME=\\\$(basename \"\\\$env\")
+    conda activate \"\\\$BASENAME\"
+    python -m ipykernel install --user --name \"\\\$BASENAME\"
+done
+
+IDLE_TIME=10800
+CONDA_PYTHON_DIR=\\\$(source /home/ec2-user/anaconda3/bin/activate /home/ec2-user/anaconda3/envs/JupyterSystemEnv && which python)
+if \\\$CONDA_PYTHON_DIR -c \"import boto3\" 2>/dev/null; then
+    PYTHON_DIR=\\\$CONDA_PYTHON_DIR
+elif /usr/bin/python -c \"import boto3\" 2>/dev/null; then
+    PYTHON_DIR='/usr/bin/python'
+else
+    echo \"No boto3 found in Python or Python3. Exiting...\"
+    exit 1
+fi
+echo \"Starting the SageMaker autostop script in cron\"
+(crontab -l 2>/dev/null; echo \"*/5 * * * * \\\$PYTHON_DIR /home/ec2-user/SageMaker/custom/autostop.py --time \\\$IDLE_TIME --ignore-connections >> /var/log/jupyter.log\") | crontab -
+EOF"
+sudo chmod +x /usr/local/bin/rc
+rc
+source ~/.bashrc
+```
+
+不加 display-name，避免重复
+```shell
+python -m ipykernel install --user --name \"\\\$BASENAME\" --display-name \"Custom (\\\$BASENAME)\"
+```
+
+
+
 ## Backup
 
 ```shell
