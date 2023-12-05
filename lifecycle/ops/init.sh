0e3a4ed07cb60409a75d3e42ee45c7557df2a8e4
Robin Long <binlon@amazon.com>
2023-12-05 16:06:04 +0800

    update SSH

--- lifecycle/ops/init.sh
+++ lifecycle/ops/init.sh
@@ -306,13 +306,23 @@ EOF
 
 source ~/.bashrc
 
-if [ -f /home/ec2-user/SageMaker/custom/id_rsa_${EKS_CLUSTER_NAME} ]
+# if [ -f /home/ec2-user/SageMaker/custom/id_rsa_${EKS_CLUSTER_NAME} ]
+# then
+#   sudo cp /home/ec2-user/SageMaker/custom/id_rsa_${EKS_CLUSTER_NAME} ~/.ssh/id_rsa
+#   chmod 400 ~/.ssh/id_rsa
+#   cp /home/ec2-user/SageMaker/custom/id_rsa_pub_${EKS_CLUSTER_NAME} ~/.ssh/id_rsa.pub
+#   # ssh-keygen -f ~/.ssh/id_rsa -y > ~/.ssh/id_rsa.pub
+# fi
+
+if [ -f /home/ec2-user/SageMaker/custom/${EKS_CLUSTER_NAME}_private_key.pem ]
 then
-  sudo cp /home/ec2-user/SageMaker/custom/id_rsa_${EKS_CLUSTER_NAME} ~/.ssh/id_rsa
-  chmod 400 ~/.ssh/id_rsa
-  cp /home/ec2-user/SageMaker/custom/id_rsa_pub_${EKS_CLUSTER_NAME} ~/.ssh/id_rsa.pub
+  sudo cp /home/ec2-user/SageMaker/custom/${EKS_CLUSTER_NAME}_private_key.pem ~/.ssh/id_rsa
+  sudo cp /home/ec2-user/SageMaker/custom/${EKS_CLUSTER_NAME}_public_key.pem ~/.ssh/id_rsa.pub
+  sudo chmod 400 ~/.ssh/id_rsa
+  sudo chown -R ec2-user:ec2-user ~/.ssh/
   # ssh-keygen -f ~/.ssh/id_rsa -y > ~/.ssh/id_rsa.pub
 fi
+
 
 echo "==============================================="
 echo "  Resource Metadata ......"
