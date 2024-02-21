9c98d5cce56a70b287bb7183af472d8c055c2a9e
Robin Long <binlon@amazon.com>
2024-02-21 09:52:10 +0800

    add krew

--- lifecycle/init.sh
+++ lifecycle/init.sh
@@ -172,6 +172,25 @@ sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 # sudo chmod +x $WORKING_DIR/docker-compose
 # $WORKING_DIR/docker-compose version
 
+# krew
+if [ ! -d $WORKING_DIR/bin/krew ]; then
+  export KREW_ROOT="$WORKING_DIR/bin/krew"
+  (
+    set -x; cd "$(mktemp -d)" &&
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+    KREW="krew-${OS}_${ARCH}" &&
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+    tar zxvf "${KREW}.tar.gz" &&
+    ./"${KREW}" install krew
+  )
+
+  export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+  kubectl krew update
+  kubectl krew install ctx # kubectx
+  kubectl krew install ns # kubens
+fi
+
 
 echo "==============================================="
 echo "  EC2 tools e.g. ec2-instance-selector ......"
@@ -296,6 +315,9 @@ alias egn='eksctl get nodegroup --cluster=\${EKS_CLUSTER_NAME}'
 alias ess='eksctl scale nodegroup --cluster=\${EKS_CLUSTER_NAME} --name=system --nodes'
 alias esn='eksctl scale nodegroup --cluster=\${EKS_CLUSTER_NAME} -n'
 alias es0='eksctl scale nodegroup --cluster=\${EKS_CLUSTER_NAME} --nodes=0 --nodes-min=0 -n'
+
+export KREW_ROOT="\$WORKING_DIR/bin/krew"
+export PATH="\${KREW_ROOT:-\$HOME/.krew}/bin:\$PATH"
 
 bashrc_files=(bashrc)
 path="/home/ec2-user/SageMaker/custom/"
