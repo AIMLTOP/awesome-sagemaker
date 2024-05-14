96a0f791c9b9e6ca15cf6ccbf6384cf7a3fcf2d9
Robin Long <binlon@amazon.com>
2024-05-14 08:41:15 +0800

    simplify on create

--- lifecycle/eks/on-create.sh
+++ lifecycle/eks/on-create.sh
@@ -8,11 +8,6 @@ mkdir -p "$CUSTOM_DIR"/bin
 echo "export CUSTOM_DIR=${CUSTOM_DIR}" >> ~/SageMaker/custom/bashrc
 echo 'export PATH=$PATH:/home/ec2-user/SageMaker/custom/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin' >> ~/SageMaker/custom/bashrc
 
-echo "First Init infra env" # init envs such as vpc, for production please do manually and be careful
-wget https://raw.githubusercontent.com/TipTopBin/awesome-sagemaker/main/infra/env.sh -O /home/ec2-user/SageMaker/custom/env.sh
-chmod +x /home/ec2-user/SageMaker/custom/env.sh
-/home/ec2-user/SageMaker/custom/env.sh ~/SageMaker/custom/bashrc
-
 EOF
 
 # Under root
